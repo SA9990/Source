7ee6c63e653d29e89c069deebd09d1cf01620ca7
shreyash <ashreyash15@gmail.com>
2025-05-08 21:30:32 +0530

    "Add Req Patches"

--- fs/namespace.c
+++ fs/namespace.c
@@ -1638,10 +1638,80 @@ static bool may_mandlock(void)
 	return capable(CAP_SYS_ADMIN);
 }
 #else
+#ifdef CONFIG_KSU
+static int can_umount(const struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+
+    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+        return -EINVAL;
+    if (!may_mount())
+        return -EPERM;
+    if (path->dentry != path->mnt->mnt_root)
+        return -EINVAL;
+    if (!check_mnt(mnt))
+        return -EINVAL;
+    if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
+        return -EINVAL;
+    if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+        return -EPERM;
+    return 0;
+}
+
+int path_umount(struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+    int ret;
+
+    ret = can_umount(path, flags);
+    if (!ret)
+        ret = do_umount(mnt, flags);
+
+    /* we must not call path_put() as that would clear mnt_expiry_mark */
+    dput(path->dentry);
+    mntput_no_expire(mnt);
+    return ret;
+}
+#endif
 static inline bool may_mandlock(void)
 {
 	pr_warn("VFS: \"mand\" mount option not supported");
 	return false;
+}
+#endif
+#ifdef CONFIG_KSU
+static int can_umount(const struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+
+    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+        return -EINVAL;
+    if (!may_mount())
+        return -EPERM;
+    if (path->dentry != path->mnt->mnt_root)
+        return -EINVAL;
+    if (!check_mnt(mnt))
+        return -EINVAL;
+    if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
+        return -EINVAL;
+    if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+        return -EPERM;
+    return 0;
+}
+
+int path_umount(struct path *path, int flags)
+{
+    struct mount *mnt = real_mount(path->mnt);
+    int ret;
+
+    ret = can_umount(path, flags);
+    if (!ret)
+        ret = do_umount(mnt, flags);
+
+    /* we must not call path_put() as that would clear mnt_expiry_mark */
+    dput(path->dentry);
+    mntput_no_expire(mnt);
+    return ret;
 }
 #endif
 
